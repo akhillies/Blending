652d20b0485080ac9ae3382803d8b708b881d155
Akhil Batra <akhilbatra@berkeley.edu>
2014-11-09 03:46:52 -0800

    abstracted people's positions

--- js/main.js
+++ js/main.js
@@ -7,8 +7,9 @@ $(document).ready(function()
         update: update
     });
 
-    var numPeople = 9;
+    var numPeople = 5;
     var peopleColors = [
+        [Math.random() * 0xffffff, Math.random() * 0xffffff, Math.random() * 0xffffff],
         [Math.random() * 0xffffff, Math.random() * 0xffffff, Math.random() * 0xffffff],
         [Math.random() * 0xffffff, Math.random() * 0xffffff, Math.random() * 0xffffff],
         [Math.random() * 0xffffff, Math.random() * 0xffffff, Math.random() * 0xffffff],
@@ -19,24 +20,39 @@ $(document).ready(function()
         [Math.random() * 0xffffff, Math.random() * 0xffffff, Math.random() * 0xffffff],
         [Math.random() * 0xffffff, Math.random() * 0xffffff, Math.random() * 0xffffff]
     ];
+    var peopleXPos = [
+        125,
+        900 / numPeople + 125,
+        1800 / numPeople + 125,
+        2700 / numPeople + 125,
+        3600 / numPeople + 125,
+        4500 / numPeople + 125,
+        5400 / numPeople + 125,
+        6300 / numPeople + 125,
+        7200 / numPeople + 125,
+        8100 / numPeople + 125
+    ];
+    var peopleYPos = 150;
     var goalColor = [255, 255, 255];
     var startColor = [100, 100, 100];
     var range = 0;
 
-    var colorConstant = 1;
-    var blockW = 50;
-    var blockH = 50;
+    var colorConstant = .5;
+    var chScale = .1;
     var moveSpeed = 2;
+    var stopAnim = 2;
+    var animOrder = [4, 3, 2, 1, 0];
+    var framesPerWalk = 18;
 
     var youBlock;
     var arrowKeys;
     var thePeople;
     var goalBlock;
-    var startBlock;
 
     function preload()
     {
-        game.load.image('youTile', 'assets/testbg.png');
+        game.load.image('goalTile', 'assets/testbg.png');
+        game.load.spritesheet('youTile', 'assets/characterSheet/characterSheet.png', 311, 772);
     }
 
     function create()
@@ -48,31 +64,29 @@ $(document).ready(function()
 
         for (var i = 0; i < numPeople; i++)
         {
-            var thePerson = thePeople.create(i * 100, Math.random() * 450, 'youTile');
+            var thePerson = thePeople.create(peopleXPos[i], peopleYPos, 'youTile');
             thePerson.tint = Phaser.Color.getColor(peopleColors[i][0], peopleColors[i][1], peopleColors[i][2]);
-            thePerson.width = blockW;
-            thePerson.height = blockH;
+            thePerson.scale.x = chScale;
+            thePerson.scale.y = chScale;
             thePerson.tintTaken = [0, 0, 0];
         }
 
-        startBlock = game.add.sprite(0, 0, 'youTile');
-        startBlock.tint = Phaser.Color.getColor(startColor[0], startColor[1], startColor[2]);
-        startBlock.width = blockW;
-        startBlock.height = blockH;
-
-        goalBlock = game.add.sprite(950, 250, 'youTile');
+        goalBlock = game.add.sprite(1000, peopleYPos, 'goalTile');
         goalBlock.tint = Phaser.Color.getColor(goalColor[0], goalColor[1], goalColor[2]);
-        goalBlock.width = blockW;
-        goalBlock.height = blockH;
+        goalBlock.scale.x = chScale;
+        goalBlock.scale.y = chScale;
         game.physics.arcade.enable(goalBlock)
 
-        youBlock = game.add.sprite(0, 0, 'youTile');
+        youBlock = game.add.sprite(0, peopleYPos, 'youTile');
         youBlock.tint = Phaser.Color.getColor(startColor[0], startColor[1], startColor[2]);
-        youBlock.width = blockW;
-        youBlock.height = blockH;
+        youBlock.scale.x = chScale;
+        youBlock.scale.y = chScale;
+        youBlock.anchor.setTo(0.5, 0);
         youBlock.rgb = [startColor[0], startColor[1], startColor[2]];
         game.physics.arcade.enable(youBlock)
         youBlock.body.collideWorldBounds = true;
+
+        youBlock.animations.add('walk', animOrder, framesPerWalk, true);
     }
 
     function update()
@@ -80,22 +94,31 @@ $(document).ready(function()
         game.physics.arcade.overlap(youBlock, thePeople, shiftColor, null, this);
         game.physics.arcade.overlap(youBlock, goalBlock, finishColor, null, this);
 
-        if (arrowKeys.up.isDown)
-        {
-            youBlock.y -= moveSpeed;
-        }
-        else if (arrowKeys.down.isDown)
-        {
-            youBlock.y += moveSpeed;
-        }
+        // if (arrowKeys.up.isDown)
+        // {
+        //     youBlock.y -= moveSpeed;
+        // }
+        // else if (arrowKeys.down.isDown)
+        // {
+        //     youBlock.y += moveSpeed;
+        // }
 
         if (arrowKeys.left.isDown)
         {
             youBlock.x -= moveSpeed;
+            youBlock.animations.play('walk');
+            youBlock.scale.x = -chScale;
         }
         else if (arrowKeys.right.isDown)
         {
             youBlock.x += moveSpeed;
+            youBlock.animations.play('walk');
+            youBlock.scale.x = chScale;
+        }
+        else
+        {
+            youBlock.animations.stop();
+            youBlock.frame = stopAnim;
         }
     }
 
