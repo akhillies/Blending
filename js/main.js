6d19e2b05ea4df0ea2b3658bd46a8e80cf052194
Akhil Batra <akhilbatra@berkeley.edu>
2014-11-08 21:58:26 -0800

    added white block to center

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
+$(document).ready(function()
+{
+    var game = new Phaser.Game(1024, 768, Phaser.AUTO, '',
+    {
+        preload: preload,
+        create: create,
+        update: update
+    });
 
-$(document).ready(function(){
-      var game = new Phaser.Game(1024, 768, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+    var youBlock;
 
-    function preload() {
-      game.load.image('bg', 'assets/testbg.png');
-      
+    function preload()
+    {
+        game.load.image('bg', 'assets/testbg.png');
+        game.load.image('youTile', 'assets/testbg.png');
+        youBlock = game.make.bitmapData();
+        youBlock.load('youTile');
+        youBlock.addToWorld(game.world.centerX, game.world.centerY, 0.5, 0.5);
+        youBlock.replaceRGB(0, 0, 0, 255, 255, 255, 255, 255);
     }
-    function create() {
-    }
-    function update() {
-    }
+
+    function create()
+    {}
+
+    function update()
+    {}
 })
